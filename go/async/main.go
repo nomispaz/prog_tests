2cc102f34329da705fc0ef426c6f78ab8449a9bf
nomispaz <nomispaz@example.com>
2025-07-11 21:55:32 +0200

    further tests

--- go/async/main.go
+++ go/async/main.go
@@ -5,75 +5,50 @@ import (
 	"time"
 )
 
-func runCmd(input chan string, output chan string, command string)  {
+func runCmd(input chan string, output chan string, command string) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic while sending:", r)
+		}
+	}()
 	// read from channel via async
-		go func() {
-			for msg := range input {
-				println("async function", msg)
-			}
-		}()
-
-		for {
-
-			println("goroutine", command)
-			time.Sleep(time.Duration(2000) * time.Millisecond)
-			output <- "from function"
+	go func() {
+		for msg := range input {
+			println("async function", msg)
 		}
+	}()
+
+	for {
+
+		println("goroutine", command)
+		time.Sleep(time.Duration(2000) * time.Millisecond)
+		output <- "from function"
+	}
 }
-
-func controlledReader(name string, output <-chan string, control <-chan struct{}, next chan<- struct{}) {
-	<-control // wait for permission
-	counter := 0
-	fmt.Println(name, "has control of output channel")
-
-	for msg := range output {
-		fmt.Println(name, "received:", msg)
-		time.Sleep(500 * time.Millisecond) // simulate work
-		counter += 1
-		if counter >= 3 {
-			fmt.Println(name, "exiting")
-	next <- struct{}{} // pass control to next reader
-
-		}
-	}
-
-	}
 
 func main() {
 	hello := "test"
 	counter := 0
 	output := make(chan string)
 	input := make(chan string)
-	
+
 	// start async function
 	go runCmd(input, output, hello)
 
+	// read from async function and display
+	go func() {
+		for msg := range output {
+			println(msg)
+		}
+	}()
+	// it is possible to spawn a second go routine that reads the same channel and sometimes takes the message instead of the first routine
+	go func() {
+		for msg := range output {
+			println("routine2", msg)
+		}
+	}()
+
 	for {
-		// // read from async function and display
-		// go func() {
-		// 	for msg := range output {
-		// 		println(msg)
-		// 	}
-		// }()
-		// // it is possible to spawn a second go routine that reads the same channel and sometimes takes the message instead of the first routine
-		// go func() {
-		// 	for msg := range output {
-		// 		println("routine2", msg)
-		// 	}
-		// }()
-
-		// control channels
-		controlA := make(chan struct{}, 1)
-		controlB := make(chan struct{}, 1)
-
-		// reader A
-		go controlledReader("ReaderA", output, controlA, controlB)
-
-		// reader B
-		go controlledReader("ReaderB", output, controlB, make(chan struct{})) // no handoff after B
-
-		// give control to ReaderA initially
-		controlA <- struct{}{}
 
 		hello = hello + fmt.Sprintf("%d", counter)
 		println(hello)
@@ -81,6 +56,8 @@ func main() {
 		time.Sleep(time.Duration(1000) * time.Millisecond)
 		input <- "2 via channel " + fmt.Sprintf("%d", counter)
 
+		// it is possible to close the channel while a goroutine is still reading. With this, the program panics. But: not the goroutine panics but the sender in this loop
+		close(output)
 		time.Sleep(time.Duration(7000) * time.Millisecond)
 		counter += 1
 	}
--- rust/async_tokio/src/main.rs
+++ rust/async_tokio/src/main.rs
@@ -1,4 +1,4 @@
-use std::{sync::{atomic::{AtomicUsize, Ordering}, Arc}, time::Duration};
+use std::{sync::Arc, time::Duration};
 
 use tokio::{
     sync::{
@@ -9,6 +9,7 @@ use tokio::{
 };
 
 async fn runCmd(tx: Sender<String>, mut rx: Receiver<String>, command: String) {
+
     tokio::spawn(async move {
         while let Some(msg) = rx.recv().await {
             println!("async function {}", msg);
@@ -18,7 +19,12 @@ async fn runCmd(tx: Sender<String>, mut rx: Receiver<String>, command: String) {
     loop {
         println!("tokioroutine {}", command);
         _ = sleep(Duration::new(2, 0)).await;
-        _ = tx.send(format!("from function")).await;
+        let check_output_chan = tx.send(format!("from function")).await;
+        match check_output_chan {
+            Ok(_) => {},
+            Err(e) => eprintln!("{}",e),
+        }
+
     }
 }
 
@@ -27,7 +33,6 @@ async fn main() {
     let mut hello = "test".to_string();
     let mut counter = 0;
     // I want to count the total number of running tokio threads
-    let counter_threads = Arc::new(AtomicUsize::new(0));
     let (tx_output, rx_output) = mpsc::channel::<String>(5);
     let (tx_input, rx_input) = mpsc::channel::<String>(5);
 
@@ -35,70 +40,62 @@ async fn main() {
 
     let _ = tokio::spawn(runCmd(tx_output, rx_input, hello.clone()));
 
-    loop {
-
-        let counter_clone = Arc::clone(&counter_threads);
-        counter_clone.fetch_add(1, Ordering::SeqCst);
-
-        let rx = rx_output_arc.clone();
-        let _ = tokio::spawn(async move {
-            let mut guard = rx.lock().await;
-            let mut counter = 0;
-            loop {
-                let msg = guard.recv().await;
-                match msg {
-                    Some(msg) => {
-                        if counter >= 3 {
-                            println!("first tokioroutine closed");
-                            drop(guard);
-                            break;
-                        }
-                        println!("{}", msg);
+    let rx = rx_output_arc.clone();
+    let _ = tokio::spawn(async move {
+        let mut guard = rx.lock().await;
+        let mut counter = 0;
+        loop {
+            let msg = guard.recv().await;
+            match msg {
+                Some(msg) => {
+                    if counter >= 3 {
+                        println!("first tokioroutine closed");
+                        drop(guard);
+                        break;
                     }
-                    _ => {}
+                    println!("{}", msg);
                 }
-                counter += 1;
+                _ => {}
             }
-            counter_clone.fetch_sub(1, Ordering::SeqCst);
-        });
+            counter += 1;
+        }
+    });
 
-        // this routine will never get the messages of rx_output since the channel is locked to the
-        // first routine! This routine only gets the channel if the guard from the other routine is
-        // dropped (done here by drop(guard) after three iterations)
-        
-        let counter_clone = Arc::clone(&counter_threads);
-        counter_clone.fetch_add(1, Ordering::SeqCst);
+    // this routine will never get the messages of rx_output since the channel is locked to the
+    // first routine! This routine only gets the channel if the guard from the other routine is
+    // dropped (done here by drop(guard) after three iterations)
 
-        let rx = rx_output_arc.clone();
-        let _ = tokio::spawn(async move {
-            let mut guard = rx.lock().await;
-            loop {
-                let msg = guard.recv().await;
-                match msg {
-                    Some(msg) => println!("routine2 {}", msg),
-                    _ => {}
-                }
-            }
-            counter_clone.fetch_sub(1, Ordering::SeqCst);
-        });
+    //let rx = rx_output_arc.clone();
+    //let _ = tokio::spawn(async move {
+    //    let mut guard = rx.lock().await;
+    //    loop {
+    //        let msg = guard.recv().await;
+    //        match msg {
+    //            Some(msg) => println!("routine2 {}", msg),
+    //            _ => {}
+    //        }
+    //    }
+    //});
 
+    loop {
         hello = format!("{}{}", hello, counter);
         println!("{}", hello);
         _ = tx_input.send(format!("1 via channel {}", counter)).await;
         _ = sleep(Duration::new(1, 0)).await;
         let send2 = tx_input.send(format!("2 via channel {}", counter)).await;
         match send2 {
-            Ok(msg) => {}
+            Ok(_) => {}
             Err(e) => eprintln!("Error while sending second message: {e}"),
         }
+
+        
+        // I can close the channel even though the runCmd function tries to send on it. In contrast
+        // to go, the send command returns an error that can be gracefully handled
+        let rx_output = rx_output_arc.clone();
+        let mut guard = rx_output.lock().await;
+        guard.close();
+        println!("input routine closed");
         _ = sleep(Duration::new(7, 0)).await;
         counter += 1;
-
-        // count and print the active threads that were spawned
-        let active = counter_threads.load(Ordering::SeqCst);
-        println!("Active tasks: {}", active);
-        if active == 0 {
-            break;
-        }
     }
 }
